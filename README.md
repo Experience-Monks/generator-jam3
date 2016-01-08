cccd3faae660ac0a0859fe8eda2f9b666db5ee87
Nick <nick@jam3.com>
2016-01-08 14:17:53 -0500

    Working on nyg build

---
+++ README.md
@@ -0,0 +1,13 @@
+# nyg-jam3
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+Jam3 project scaffold based on nyg
+
+## Usage
+
+[![NPM](https://nodei.co/npm/nyg-jam3.png)](https://www.npmjs.com/package/nyg-jam3)
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/njam3/nyg-jam3/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,79 @@
+var nyg = require('nyg');
+var prompts = [{
+  type: "input",
+  name: "author",
+  message: "What is your name? (Author)",
+  default: "Jam3"
+}, {
+  type: "input",
+  name: "email",
+  message: "What is your email? (Author Email)",
+  default: "td@jam3.com"
+}, {
+  type: "input",
+  name: "description",
+  message: "Describe the project:",
+  default: "A Jam3 project"
+}, {
+  type: "input",
+  name: "repo",
+  message: "What is your git repository? (GitHub Repository)",
+  default: ""
+}, {
+  type: "confirm",
+  name: "sectionNames",
+  message: "Would you perfer Landing/Landing.js over Landing/index.js?",
+  default: false
+}, {
+  type: "confirm",
+  name: "useES6",
+  message: "Would you like to use ES6?",
+  default: false
+}, {
+  type: "confirm",
+  name: "useTP",
+  message: "Would you like to use TexturePacker? (Beta)",
+  default: false
+}, {
+  type: "list",
+  message: "What framework will your project use?",
+  name: "framework",
+  choices: [{
+    name: "Bigwheel / Handlebars",
+    value: "bigwheel",
+    checked: true
+  }, {
+    name: "React",
+    value: "react"
+  }, {
+    name: "Angular",
+    value: "Angular"
+  }]
+},{
+  type: "list",
+  message: "What css preprocessor will your project use?",
+  name: "css",
+  choices: [{
+    name: "SCSS",
+    value: "scss",
+    checked: true
+  },{
+    name: "LESS",
+    value: "less"
+  }]
+}];
+var globs = [
+{
+  base: 'template/{{framework}}/',
+  glob: '**/*'
+},
+{
+  base: 'template/scripts/',
+  glob: '*'
+},
+{
+  base: 'template/scripts/{{css}}/',
+  glob: '*'
+}
+];
+nyg(prompts,globs).run();
