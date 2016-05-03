d91aeb4e7fffddbfce8257d013ed9278d2982aae
Nick <nick@jam3.com>
2016-05-03 13:57:21 -0400

    fixed envify and bigwheel builds

--- templates/sections/bigwheel/normal/index.js
+++ templates/sections/bigwheel/normal/index.js
@@ -10,7 +10,7 @@ class {{section}} {
     
   }{{else}}
 function {{section}}() {}
-{{section}}.prototype = {{{/if}}
+{{section}}.prototype = { {{/if}}
   {{#if useES6}}init{{else}}init: function{{/if}}(req, done) {
     this.dom = domify(hbs.compile(fs.readFileSync(__dirname + '/template.hbs', 'utf8'))(model[req.route]));
     document.body.appendChild(this.dom);
@@ -18,20 +18,20 @@ function {{section}}() {}
       opacity: 0
     });
     done();
-  }{{#if useES6|falsy}},{{/if}}
-  {{#if useES6}}resize{{else}}resize: function{{/if}}(w, h) {}{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
+  {{#if useES6}}resize{{else}}resize: function{{/if}}(w, h) {}{{#unless useES6}},{{/unless}}
   {{#if useES6}}animateIn{{else}}animateIn: function{{/if}}(req, done) {
     Tween.to(this.dom, 1, {
       opacity: 1,
       onComplete: done
     });
-  }{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
   {{#if useES6}}animateOut{{else}}animateOut: function{{/if}}(req, done) {
     Tween.to(this.dom, 1, {
       opacity: 0,
       onComplete: done
     });
-  }{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
   {{#if useES6}}destroy{{else}}destroy: function{{/if}}(req, done) {
     this.dom.parentNode.removeChild(this.dom);
     done();
--- templates/sections/bigwheel/preloader/index.js
+++ templates/sections/bigwheel/preloader/index.js
@@ -12,7 +12,7 @@ class Preloader {
 function Preloader(onComplete) {
   this.preloaded = onComplete;
 }
-Preloader.prototype = {{{/if}}
+Preloader.prototype = { {{/if}}
   {{#if useES6}}init{{else}}init: function{{/if}}(req, done) {
     this.dom = domify(hbs.compile(fs.readFileSync(__dirname + '/template.hbs', 'utf8'))());
     document.body.appendChild(this.dom);
@@ -20,21 +20,21 @@ Preloader.prototype = {{{/if}}
       opacity: 0
     });
     done();
-  }{{#if useES6|falsy}},{{/if}}
-  {{#if useES6}}resize{{else}}resize: function{{/if}}(w, h) {}{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
+  {{#if useES6}}resize{{else}}resize: function{{/if}}(w, h) {}{{#unless useES6}},{{/unless}}
   {{#if useES6}}animateIn{{else}}animateIn: function{{/if}}(req, done) {
     Tween.to(this.dom, 1, {
       opacity: 1,
       onComplete: done
     });
     this.preloaded();
-  }{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
   {{#if useES6}}animateOut{{else}}animateOut: function{{/if}}(req, done) {
     Tween.to(this.dom, 1, {
       opacity: 0,
       onComplete: done
     });
-  }{{#if useES6|falsy}},{{/if}}
+  }{{#unless useES6}},{{/unless}}
   {{#if useES6}}destroy{{else}}destroy: function{{/if}}(req, done) {
     this.dom.parentNode.removeChild(this.dom);
     done();
