35dd74d6567b2931520229abbfa24ee682dc215b
Nick <nick@jam3.com>
2016-07-05 15:59:17 -0400

    Fixes #215

--- templates/scripts/release.js
+++ templates/scripts/release.js
@@ -16,7 +16,7 @@ var post = function(file,src) {
   mkdirp(config.output,function(err) {
     if (!err) {
       try {
-        var min = uglify.minify(src.toString(),{fromString: true});
+        var min = uglify.minify(src.toString(),{fromString: true, compress: {drop_console: true}});
       } catch(err) {
         fs.writeFile(path.join(config.output,file),src.toString(),function(e) {
           console.log('\x1b[31m Error at line',err.line+':',err.message+'\x1b[0m');
