18c40ac6432dc05b5b4e44897199e4fb113e8b39
Nick <nick@jam3.com>
2016-07-17 22:39:45 -0400

    Updated

--- templates/unsupported/php/index.php
+++ templates/unsupported/php/index.php
@@ -1,6 +1,6 @@
 <?php
-require('Browser.php');
-require('Mobile_Detect.php');
+require('lib/Browser.php');
+require('lib/Mobile_Detect.php');
 
 $browser = new Browser();
 $devices = json_decode(file_get_contents('device-matrix.json'), true);
