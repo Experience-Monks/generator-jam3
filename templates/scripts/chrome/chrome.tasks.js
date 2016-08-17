7b8f3c03c50de2e97aa4e593a8243709332c5810
nascherman <n_scherman@hotmail.com>
2016-08-17 18:07:58 -0400

    chrome dev scripts and copying

--- templates/scripts/chrome/chrome.tasks.js
+++ templates/scripts/chrome/chrome.tasks.js
@@ -6,13 +6,13 @@ module.exports = function(type, cb) {
   const env = type === 'build' ? 'prod' : type;
   rm('-rf', type);
   mkdir(type);
-  browserify('templates/chrome/extension/background.js').bundle(function(err, buff) {
-    fs.writeFileSync('templates/chrome/background.bundle.js', buff);
-    cp(`templates/chrome/manifest.${env}.json`, `${type}/manifest.json`);
-    cp('-R', 'templates/chrome/assets/*', type);
-    cp('-R', 'templates/chrome/content_script.js', type);
-    cp('templates/chrome/background.bundle.js', type);
-    exec(`jade -O "{ env: '${env}' }" -o ${type} templates/chrome/views/`);
+  browserify('chrome/extension/background.js').bundle(function(err, buff) {
+    fs.writeFileSync('chrome/background.bundle.js', buff);
+    cp(`chrome/manifest.${env === 'prod' ? env : 'dev'}.json`, `${type}/manifest.json`);
+    cp('-R', 'chrome/assets/*', type);
+    cp('-R', 'chrome/content_script.js', type);
+    cp('chrome/background.bundle.js', type);
+    exec(`jade -O "{ env: '${env}' }" -o ${type} chrome/views/`);
     cb();
   });
 };
