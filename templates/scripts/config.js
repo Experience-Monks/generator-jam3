b5c0889419490102e63d76f3dd8575f1069c81d7
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2016-09-02 14:19:25 -0400

    fix recursive merge

--- templates/scripts/config.js
+++ templates/scripts/config.js
@@ -3,6 +3,6 @@ var merge = require('merge');
 var argv = require('minimist')(process.argv.slice(2));
 var type = argv.env || process.env.NODE_ENV || 'development';
 var configs = require('../config.json');
-var config = merge(true, configs.defaults || {}, configs[type]);
+var config = merge.recursive(configs.defaults, configs[type]);
 config.NODE_ENV = type;
 module.exports = config;
