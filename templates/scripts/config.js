90ee967b5930a59c708e49916d0e36bd6f37724e
Nick <nick@jam3.com>
2017-01-02 18:45:24 -0500

    Fixed main.php Removed moment requirement Move timstamp logic to config.js

--- templates/scripts/config.js
+++ templates/scripts/config.js
@@ -1,4 +1,5 @@
 'use strict';
+var fs = require('fs');
 var merge = require('merge');
 var argv = require('minimist')(process.argv.slice(2));
 var type = argv.env || process.env.NODE_ENV || 'development';
@@ -6,8 +7,21 @@ var configs = require('../config.json');
 var config = merge.recursive(configs.defaults, configs[type]);
 var path = require('path');
 
-if (type === 'development') {
-  config.stylesheet = path.parse(config.style).name + '.css';
+config.stylesheet = path.parse(config.style).name + '.css';
+
+if (type==='production' && config.timestamp) {
+  var delimiter = '-';
+  var stamp = typeof config.timestamp === 'string' ? config.timestamp : fs.readFileSync('timestamp.txt');
+  if (stamp) {
+    var stamp = delimiter + stamp;
+    config.stylesheet = path.parse(config.style).name.split(delimiter)[0] + stamp + '.css';
+
+    config.bundle = path.parse(config.bundle).name.split(delimiter)[0] + stamp + '.js';
+    if (config.vendor) config.vendor = path.parse(config.vendor).name.split(delimiter)[0] + stamp + '.js';
+    
+    var parsed = path.parse(config.ASSET_PATH);
+    config.ASSET_PATH = path.join(parsed.dir, parsed.base+stamp);
+  }
 }
 
 config.NODE_ENV = type;
--- templates/scripts/timestamp.js
+++ templates/scripts/timestamp.js
@@ -1,28 +1,14 @@
 'use strict';
-var moment = require('moment');
-var configs = require('../config.json');
-var path = require('path');
 var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
 
-var delimiter = '_';
-var timestamp = moment().format().substring(0, 19).replace('T', delimiter);
-var postfix = delimiter + timestamp;
-
-// css files
-var style = configs.production.style || configs.defaults.style;
-configs.production.stylesheet = path.parse(style).name.split(delimiter)[0] + postfix + '.css';
-
-// js files
-var bundle = configs.production.bundle || configs.defaults.bundle;
-var vendor = configs.production.vendor || configs.defaults.vendor;
-configs.production.bundle = path.parse(bundle).name.split(delimiter)[0] + postfix + '.js';
-
-if (vendor) {
-  configs.production.vendor = path.parse(vendor).name.split(delimiter)[0] + postfix + '.js';
+function getTimestamp() {
+  var str = new Date().toISOString().substring(0,19);
+  return str.replace(/-/g,'').replace('T','-').replace(/:/g,'');
 }
 
-// assets
-configs.production.ASSET_PATH = './assets' + postfix + '/';
-
-fs.writeFileSync(path.join(__dirname, '..', 'config.json'), JSON.stringify(configs, null, 2));
-//console.log(configs)
+if (argv.delete) {
+  fs.unlinkSync('timestamp.txt');
+} else {
+  fs.writeFileSync('timestamp.txt',getTimestamp());
+}
