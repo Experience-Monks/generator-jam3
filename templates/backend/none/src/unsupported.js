d57e42dc9e9b7c002f3c6c00bb3580df06c811c4
Nick <nick@jam3.com>
2017-03-10 15:52:31 -0500

    Updated unsupported page

--- templates/backend/none/src/unsupported.js
+++ templates/backend/none/src/unsupported.js
@@ -7,11 +7,11 @@ let os = device.os().toLocaleLowerCase();
 let osVersion = parseFloat(device.osVersion());
 if (os) os = os.toLowerCase();
 if (osVersion==='Unknown') osVersion = Number.MAX_SAFE_INTEGER || 9999;
-if (browser.checkBot() || browser.checkFacebook()) {
+if (browser.checkBot()) {
   supported = true;
 } else if (os === 'android' || os === 'ios') {
-  if (os==='ios' && browser.checkSafari() && osVersion>=matrix.ios) supported = true;
-  if (os==='android' && browser.checkChrome() && osVersion>=matrix.android && version>=matrix.chrome) supported = true;
+  if (os==='ios' && (browser.checkSafari() || browser.checkFacebook()) && osVersion>=matrix.ios) supported = true;
+  if (os==='android' && (browser.checkChrome() || browser.checkFacebook()) && osVersion>=matrix.android && version>=matrix.chrome) supported = true;
 } else if (browser.checkIE() && version>=matrix.ie ||
     browser.checkFirefox() && version>=matrix.firefox ||
     browser.checkChrome() && version>=matrix.chrome ||
