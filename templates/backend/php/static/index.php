1e6a95e63b6d8c390c525a483e4d3d460b9f9591
Nick <nick@jam3.com>
2017-05-11 15:57:52 -0400

    vendor fixes

--- templates/backend/php/static/index.php
+++ templates/backend/php/static/index.php
@@ -11,8 +11,8 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, shrink-to-fit=no">
   <link rel="stylesheet" type="text/css" href="\{{stylesheet}}">
 </head>
-  <body>\{{#is NODE_ENV "production"}}\{{#if vendor}}
-  <script type="text/javascript" src="\{{vendor}}"></script>\{{/if}}\{{/is}}
+  <body>\{{#if vendor}}
+  <script type="text/javascript" src="\{{vendor}}"></script>\{{/if}}
   <script type="text/javascript" src="\{{bundle}}"></script>
   </body>
 </html>
