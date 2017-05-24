94aae41b7e757a1cd62b160d663dbeaf0eb1d323
Nick <nick@jam3.com>
2017-05-23 21:52:20 -0400

    Working on unsupported tiers

--- templates/unsupported/php/static/index.php
+++ templates/unsupported/php/static/index.php
@@ -1,9 +1,18 @@
 <?php
 require('lib/Browser.php');
-require('lib/Mobile_Detect.php');
 
 $browser = new Browser();
 $devices = json_decode(file_get_contents('device-matrix.json'), true);
+$lookup = array(
+  'ie' => Browser::BROWSER_IE,
+  'safari' => Browser::BROWSER_SAFARI,
+  'chrome' => Browser::BROWSER_CHROME,
+  'firefox' => Browser::BROWSER_FIREFOX,
+  'opera' => Browser::BROWSER_OPERA,
+  'ios' => array(Browser::PLATFORM_IPHONE, Browser::PLATFORM_IPAD),
+  'android' => Browser::PLATFORM_ANDROID
+);
+
 
 if (
   $browser->getBrowser() == Browser::BROWSER_IE && $browser->getVersion() < $devices['ie']
