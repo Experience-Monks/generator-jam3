1f9795616b1e9f87929b7314152a1cabb188b235
vadim <vadim@jam3.com>
2017-09-30 01:23:35 -0400

    js animations

--- templates/react/src/components/HamburgerButton/index.js
+++ templates/react/src/components/HamburgerButton/index.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import animate from 'gsap';
 
-const states = {
+const STATES = {
   idle: 'idle',
   close: 'close',
   back: 'back',
@@ -16,27 +17,74 @@ export default class HamburgerButton extends PureComponent {
     };
   }
 
+  componentDidMount() {
+    this.bars = [...this.container.querySelectorAll('.bar')];
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.buttonState !== this.props.buttonState) {
       this.setState({buttonState: nextProps.buttonState});
     }
+
     if (nextProps.isMouseOver !== this.props.isMouseOver) {
       this.setState({isMouseOver: nextProps.isMouseOver});
     }
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.buttonState !== this.state.buttonState) {
+      if (this.state.buttonState === STATES.close) {
+        this.goToCloseState();
+      } else if (this.state.buttonState === STATES.back) {
+        this.goToBackState();
+      } else {
+        this.goToIdleState();
+      }
+    }
+  }
+
+  checkIsIdleState = () => {
+    return (this.state.buttonState !== STATES.close && this.state.buttonState !== STATES.back);
+  };
+
+  goToCloseState = () => {
+    const duration = 0.2;
+    animate.killTweensOf(this.bars);
+    animate.to(this.bars[0], duration, {rotation: 45, x: 1, y: 0});
+    animate.to(this.bars[1], duration, {scaleX: 0, autoAlpha: 0});
+    animate.to(this.bars[2], duration, {rotation: -45, y: 0});
+  };
+
+  goToBackState = () => {
+    const duration = 0.2;
+    animate.killTweensOf(this.bars);
+    animate.to(this.bars[0], duration, {x: -1, y: 10, rotation: -45, scaleX: 0.8});
+    animate.to(this.bars[1], duration, {scaleX: 0, autoAlpha: 0});
+    animate.to(this.bars[2], duration, {x: 1, y: -9, rotation: 45, scaleX: 0.8});
+  };
+
+  goToIdleState = () => {
+    animate.killTweensOf(this.bars);
+    animate.to(this.bars, 0.2, {x: 0, y: 0, rotation: 0, scaleX: 1, autoAlpha: 1});
+  };
+
   handleClick = (e) => {
-    const buttonState = this.state.buttonState === states.idle ? this.props.activeState : states.idle;
+    const buttonState = this.state.buttonState === STATES.idle ? this.props.activeState : STATES.idle;
     this.setState({buttonState});
     this.props.onClick(buttonState);
   };
 
   handleMouseEnter = (e) => {
+    if (this.checkIsIdleState()) {
+      animate.killTweensOf(this.bars);
+      animate.to([this.bars[1], this.bars[3]], 0.2, {scaleX: 0.8});
+    }
     this.setState({isMouseOver: true});
     this.props.onMouseEnter();
   };
 
   handleMouseLeave = (e) => {
+    this.checkIsIdleState() && this.goToIdleState();
     this.setState({isMouseOver: false});
     this.props.onMouseLeave();
   };
@@ -46,7 +94,7 @@ export default class HamburgerButton extends PureComponent {
     const state = this.state;
 
     const style = Object.assign({}, this.props.style);
-    const buttonClass = `HamburgerButton ${state.buttonState} ${state.isMouseOver ? 'hover' : ''} ${props.className} `;
+    const buttonClass = `HamburgerButton ${props.className}`;
 
     return (
       <button
@@ -60,7 +108,7 @@ export default class HamburgerButton extends PureComponent {
       >
         <div className="bars-container">
           {
-            [0, 1, 2].map(item => <span key={item} className={`bar ${item}`}/>)
+            [0, 1, 2].map(item => <div key={item} className={`bar ${item}`}/>)
           }
         </div>
       </button>
@@ -84,8 +132,8 @@ HamburgerButton.defaultProps = {
   className: '',
   style: {},
   tabIndex: 0,
-  state: states.idle,
-  activeState: states.close,
+  state: STATES.idle,
+  activeState: STATES.close,
   isMouseOver: false,
   onClick: () => f => f,
   onMouseEnter: f => f,
