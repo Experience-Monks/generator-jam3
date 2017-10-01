27f163ff7232819b7a86e52985a5a8a2551902cf
vadim <vadim@jam3.com>
2017-10-01 14:04:05 -0400

    fixing path

--- templates/react/src/components/MobileFullscreenVideo/index.js
+++ templates/react/src/components/MobileFullscreenVideo/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import fullScreen from 'fullscreen-handler';
 
-import detect from '../../utils/detect';
+import detect from '../../util/detect';
 
 const isIOS = detect.isIOS;
 
