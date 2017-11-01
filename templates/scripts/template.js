8810ee324948d12abe962686c7dd3ea6467b397a
miken <miken@thesecretlocation.com>
2017-11-01 17:06:35 -0400

    Support for handlebar partials, for no-framework builds at least

--- templates/scripts/template.js
+++ templates/scripts/template.js
@@ -1,4 +1,9 @@
 var hbs = require('handlebars');
+var path = require('path');
+var fs = require('fs');
+var walk = require('walk').walk;
+
+
 hbs.registerHelper('is', function(variable,test,options) {
   if (variable !== test) {
     return options.inverse(this);
@@ -6,6 +11,38 @@ hbs.registerHelper('is', function(variable,test,options) {
     return options.fn(this);
   }
 });
+
+
+//
+// Mostly taken from hbs package
+//
+registerPartials = function (directory, done) {
+
+  var register = function(filepath, done) {
+    var isValidTemplate = /\.(html|hbs)$/.test(filepath);
+
+    if (!isValidTemplate) {
+      return done(null);
+    }
+
+    fs.readFile(filepath, 'utf8', function(err, data) {
+      if (!err) {
+        var ext = path.extname(filepath);
+        var templateName = path.relative(directory, filepath)
+          .slice(0, -(ext.length)).replace(/[ -]/g, '_').replace('\\', '/');
+        hbs.registerPartial(templateName, data);
+      }
+      done(err);
+    });
+  };
+
+  walk(directory).on('file', function(root, stat, next) {
+    register(path.join(root, stat.name), next);
+  }).on('end', done || function() {});
+
+};
+registerPartials("./src/partials/");
+
 
 module.exports = function(template,data) {
   var render = hbs.compile(template);
